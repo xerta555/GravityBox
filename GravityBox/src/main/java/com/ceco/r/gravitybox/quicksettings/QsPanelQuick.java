5428197c311d1ebf95e6ed47f261028ace0f40f2
C3C0 <xgravitybox@gmail.com>
2021-01-03 10:11:24 +0100

    QS: fixed Tiles per header option

--- GravityBox/src/main/java/com/ceco/r/gravitybox/quicksettings/QsPanelQuick.java
+++ GravityBox/src/main/java/com/ceco/r/gravitybox/quicksettings/QsPanelQuick.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Peter Gregus for GravityBox Project (C3C076@xda)
+ * Copyright (C) 2021 Peter Gregus for GravityBox Project (C3C076@xda)
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,7 +16,6 @@ package com.ceco.r.gravitybox.quicksettings;
 
 import com.ceco.r.gravitybox.GravityBox;
 import com.ceco.r.gravitybox.GravityBoxSettings;
-import com.ceco.r.gravitybox.Utils;
 import com.ceco.r.gravitybox.managers.BroadcastMediator;
 import com.ceco.r.gravitybox.managers.SysUiManagers;
 
@@ -34,13 +33,12 @@ public class QsPanelQuick implements BroadcastMediator.Receiver {
     private static final boolean DEBUG = false;
 
     private static final String CLASS_QS_PANEL_QUICK = "com.android.systemui.qs.QuickQSPanel";
-    private static final String CLASS_TUNER_SERVICE_IMPL = "com.android.systemui.tuner.TunerServiceImpl";
 
     private static void log(String message) {
         XposedBridge.log(TAG + ": " + message);
     }
 
-    private int mNumTilesOrig;
+    private Integer mNumTilesDefault;
     private int mNumTiles;
     private View mPanel;
 
@@ -55,7 +53,7 @@ public class QsPanelQuick implements BroadcastMediator.Receiver {
     }
 
     private void initPreferences(XSharedPreferences prefs) {
-        mNumTiles = Integer.valueOf(prefs.getString(
+        mNumTiles = Integer.parseInt(prefs.getString(
                 GravityBoxSettings.PREF_KEY_QUICK_SETTINGS_TILES_PER_HEADER, "0"));
     }
 
@@ -63,37 +61,25 @@ public class QsPanelQuick implements BroadcastMediator.Receiver {
     public void onBroadcastReceived(Context context, Intent intent) {
         if (intent.hasExtra(GravityBoxSettings.EXTRA_QS_COLS_HEADER)) {
             mNumTiles = intent.getIntExtra(GravityBoxSettings.EXTRA_QS_COLS_HEADER, 0);
+            if (DEBUG) log("onBroadcastReceived: mNumTiles=" + mNumTiles);
             updateMaxTiles();
         }
     }
 
     private void createHooks(ClassLoader cl) {
         try {
-            XposedBridge.hookAllConstructors(XposedHelpers.findClass(
-                    CLASS_QS_PANEL_QUICK, cl), new XC_MethodHook() {
+            XposedHelpers.findAndHookMethod(CLASS_QS_PANEL_QUICK, cl,
+                    "setMaxTiles", int.class, new XC_MethodHook() {
                 @Override
-                protected void afterHookedMethod(MethodHookParam param) {
+                protected void beforeHookedMethod(MethodHookParam param) {
                     mPanel = (View) param.thisObject;
-                    if (DEBUG) log("QuickQSPanel constructed");
-                    if (Utils.isSamsungRom()) {
-                        updateMaxTiles();
+                    if (mNumTilesDefault == null) {
+                        mNumTilesDefault = (int) param.args[0];
                     }
-                }
-            });
-    
-            XposedHelpers.findAndHookMethod(CLASS_TUNER_SERVICE_IMPL, cl,
-                    "getValue", String.class, int.class, new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) {
-                    if ("sysui_qqs_count".equals(param.args[0])) {
-                        mNumTilesOrig = (int) param.getResult();
-                        if (DEBUG) log("TunerServiceImpl: getValue: key=" + param.args[0] +
-                                "; originalValue=" + mNumTilesOrig +
-                                "; newValue=" + mNumTiles);
-                        if (mNumTiles > 0) {
-                            param.setResult(mNumTiles);
-                        }
+                    if (mNumTiles > 0) {
+                        param.args[0] = mNumTiles;
                     }
+                    if (DEBUG) log("QuickQsPanel: setMaxTiles: value=" + param.args[0]);
                 }
             });
         } catch (Throwable t) {
@@ -104,9 +90,9 @@ public class QsPanelQuick implements BroadcastMediator.Receiver {
     private void updateMaxTiles() {
         try {
             if (mPanel != null) {
-                Object tunable = XposedHelpers.getObjectField(mPanel, "mNumTiles");
-                XposedHelpers.callMethod(tunable, "onTuningChanged",
-                        "sysui_qqs_count", String.valueOf(mNumTiles));
+                XposedHelpers.callMethod(mPanel, "setMaxTiles",
+                        (mNumTiles == 0 && mNumTilesDefault != null ?
+                            mNumTilesDefault : mNumTiles));
                 if (DEBUG) log("Number of header tiles updated");
             }
         } catch (Throwable t) {
